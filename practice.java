d3dfdb64d48a21be814b8f4522e5c5a6ea3e575c
Khauzen2 <khauzenmavics@gmail.com>
2025-06-16 07:57:50 +0200

    continuing code

--- practice.java
+++ practice.java
@@ -8,6 +8,8 @@ public class practice {
         String[] sports = new String[4];
         String[] provinces = new String[9];
         String[] seasons = new String[4];
+        String[] daysOfWeek = new String[7];
+        String[] months = new String[12];
         Scanner input = new Scanner(System.in);
         System.out.println("My first fruit choice is " + fruits[0]);
         System.out.println("My second fruit choice is " + fruits[1]);
@@ -107,5 +109,70 @@ public class practice {
         System.out.println("Second season is " + seasons[1]);
         System.out.println("Third season is " + seasons[2]);
         System.out.println("Third season is " + seasons[3]);
+
+        // days of the week input output
+        System.out.println("Enter first day of the week");
+        daysOfWeek[0] = input.nextLine();
+        System.out.println("Enter second day of the week");
+        daysOfWeek[1] = input.nextLine();
+        System.out.println("Enter third day of the week");
+        daysOfWeek[2] = input.nextLine();
+        System.out.println("Enter fourth day of the week");
+        daysOfWeek[3] = input.nextLine();
+        System.out.println("Enter fith day of the week");
+        daysOfWeek[4] = input.nextLine();
+        System.out.println("Enter sixth day of the week");
+        daysOfWeek[5] = input.nextLine();
+        System.out.println("Enter seventh day of the week");
+        daysOfWeek[6] = input.nextLine();
+
+        // output
+        System.out.println("The first day of the week is " + daysOfWeek[0]);
+        System.out.println("The second day of the week is " + daysOfWeek[1]);
+        System.out.println("The third day of the week is " + daysOfWeek[2]);
+        System.out.println("The fourth day of the week is " + daysOfWeek[3]);
+        System.out.println("The fith day of the week is " + daysOfWeek[4]);
+        System.out.println("The sixth day of the week is " + daysOfWeek[5]);
+        System.out.println("The seventh day of the week is " + daysOfWeek[6]);
+
+        // months input output
+        System.out.println("Enter first month");
+        months[0] = input.nextLine();
+        System.out.println("Enter second month");
+        months[1] = input.nextLine();
+        System.out.println("Enter third month");
+        months[2] = input.nextLine();
+        System.out.println("Enter fourth month");
+        months[3] = input.nextLine();
+        System.out.println("Enter fith month");
+        months[4] = input.nextLine();
+        System.out.println("Enter sixth month");
+        months[5] = input.nextLine();
+        System.out.println("Enter seventh month");
+        months[6] = input.nextLine();
+        System.out.println("Enter eighth month");
+        months[7] = input.nextLine();
+        System.out.println("Enter nineth month");
+        months[8] = input.nextLine();
+        System.out.println("Enter tenth month");
+        months[9] = input.nextLine();
+        System.out.println("Enter eleventh month");
+        months[10] = input.nextLine();
+        System.out.println("Enter twenth month");
+        months[11] = input.nextLine();
+
+        // output
+        System.out.println("The first month is " + months[0]);
+        System.out.println("The second month is " + months[1]);
+        System.out.println("The third month is " + months[2]);
+        System.out.println("The fourth month is " + months[3]);
+        System.out.println("The fifth month is " + months[4]);
+        System.out.println("The sixth month is " + months[5]);
+        System.out.println("The seventh month is " + months[6]);
+        System.out.println("The eighth month is " + months[7]);
+        System.out.println("The nineth month is " + months[8]);
+        System.out.println("The tenth month is " + months[9]);
+        System.out.println("The eleventh month is " + months[10]);
+        System.out.println("The twelveth month is " + months[11]);
     }
 }
