96221066565fd64060c1b104330196cb31687bae
Khauzen2 <khauzenmavics@gmail.com>
2025-06-19 18:49:59 +0200

    printing to be continued

---
+++ MusicPlayerActivity.java
@@ -0,0 +1,26 @@
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private ArrayList<Song> songList;
+    private int currentPosition = 0;
+
+    @Override
+    protected void onCreate(Bundle saveInstancesState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+
+        // Initialize player and load songs
+        mediaPlayer = new MediaPlayer();
+        loadSongs();
+
+        // Setup play button
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(v -> playSong());
+    }
+
+    private void loadSongs() {
+        System.out.println("To be continued!!!");
+
+    }
+
+}
